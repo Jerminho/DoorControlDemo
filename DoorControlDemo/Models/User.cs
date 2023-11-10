74ecc221a95560397e4c0559c12932b3df4a913b
Pierre Njiné Happy <71798532+pgm-pierrejeremie@users.noreply.github.com>
2023-11-10 07:26:37 +0100

    Add project files.

---
+++ DoorControlDemo/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO.Packaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorControlDemo.Models
+{
+    internal class User
+    {
+        [Required(ErrorMessage = "Id is required")]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Name is required. Provide a name to this user.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Badge is required. Assign a badge to this user.")]
+        Badge Badge { get; set; }
+
+        public string? Mail { get; set; }
+
+        //A phone number can hold max 15 numbers
+        [StringLength(15)]
+        public string? PhoneNumber { get; set; }
+    }
+}
+
